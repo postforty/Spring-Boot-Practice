023c520b668c9401c3bae42ee98f7f178444d719
postforty <75767073+postforty@users.noreply.github.com>
2022-08-03 21:59:10 +0900

    [ADD] GET API - queryParam

--- springboot/demo/src/main/java/com/example/demo/controller/GetApiController.java
+++ springboot/demo/src/main/java/com/example/demo/controller/GetApiController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +34,35 @@ public class GetApiController {
 	
 	// ?key=value&key2=value2
 	// http://localhost:9090/api/get/query-param?user=steve&email=steve@gmail.com&age=40
+	// Map은 모든 것을 받을 수 있다.
+	@GetMapping(path="query-param")
+	public String queryParam(@RequestParam Map<String, String> queryParam) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		queryParam.entrySet().forEach(entry -> {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+			System.out.println("\n");
+			
+			sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+		});
+		
+		return sb.toString();
+	}
 	
+	// 명시적으로 받을 수 있다.
+	@GetMapping(path="query-param02")
+	public String queryParam02(
+			@RequestParam String name,
+			@RequestParam String email,
+			@RequestParam int age
+	) {
+
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(age);
+		
+		return name+" "+email+" "+age;
+	}
 }
