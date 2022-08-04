7a3b6660cd163a20264968bf760d0fb6813d9229
postforty <75767073+postforty@users.noreply.github.com>
2022-08-05 00:58:15 +0900

    [INITIAL] RestTemplate

---
+++ springboot/client/src/main/java/com/example/demo/service/RestTemplateService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class RestTemplateService {
+	
+	// http://localhost/api/server/hello
+	// response
+	public String hello() {
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:9090")
+				.path("/api/server/hello")
+				.encode()
+				.build()
+				.toUri();
+		System.out.println(uri.toString());
+		
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+		
+		System.out.println(result.getStatusCode());
+		System.out.println(result.getBody());
+		
+		return result.getBody();
+		
+	}
+
+}
