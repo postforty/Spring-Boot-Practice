cd6c3f748e369a6b1475b1c30608612635b084fd
postforty <75767073+postforty@users.noreply.github.com>
2022-08-04 15:09:21 +0900

    [UPDATE] POST API - POST DTO

--- springboot/demo/src/main/java/com/example/demo/controller/PostApiController.java
+++ springboot/demo/src/main/java/com/example/demo/controller/PostApiController.java
@@ -1,24 +1,29 @@
 package com.example.demo.controller;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.PostRequestDto;
+
 @RestController
 @RequestMapping("/api")
 public class PostApiController {
 	
+//	@PostMapping("/post")
+//	public void post(@RequestBody Map<String, Object> requestData) {
+//		
+//		requestData.entrySet().forEach(stringObjectEntry -> {
+//			System.out.println("key : "+stringObjectEntry.getKey());
+//			System.out.println("value : "+stringObjectEntry.getValue());
+//		});
+//		
+//	}
 	@PostMapping("/post")
-	public void post(@RequestBody Map<String, Object> requestData) {
+	public void post(@RequestBody PostRequestDto requestData) {
 		
-		requestData.entrySet().forEach(stringObjectEntry -> {
-			System.out.println("key : "+stringObjectEntry.getKey());
-			System.out.println("value : "+stringObjectEntry.getValue());
-		});
+		System.out.println(requestData);
 		
 	}
-
 }
