0ca00a3e658cfdd362f7626bc8b31d69e7628351
postforty <75767073+postforty@users.noreply.github.com>
2022-08-05 01:19:30 +0900

    [UPDATE] RestTemplate - GET

--- springboot/server/src/main/java/com/example/demo/controller/ServerApiController.java
+++ springboot/server/src/main/java/com/example/demo/controller/ServerApiController.java
@@ -1,19 +1,21 @@
 package com.example.demo.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.User;
 
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
 	
 	@GetMapping("/hello")
-	public User hello() {
+	public User hello(@RequestParam String name, @RequestParam int age) {
 		User user = new User();
-		user.setName("steve");
-//		user.setAge(10);
+		user.setName(name);
+		user.setAge(age);
 		return user;
 	}
 
