4e484cd6d9bc48a49bb63f4683895f3cbc4e33f8
postforty <75767073+postforty@users.noreply.github.com>
2022-08-06 05:19:04 +0900

    [UPDATE] 네이버 API 연동

--- springboot/server/src/main/java/com/example/demo/controller/ServerApiController.java
+++ springboot/server/src/main/java/com/example/demo/controller/ServerApiController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpEntity;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.dto.Req;
 import com.example.demo.dto.User;
@@ -19,6 +27,34 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+	
+	@GetMapping("/naver")
+	public String naver() {
+		
+		URI uri = UriComponentsBuilder
+				.fromUriString("https://openapi.naver.com")
+				.path("/v1/search/blog.json")
+				.queryParam("query", "°¥ºñÁý")
+				.queryParam("display", 10)
+				.queryParam("start", 1)
+				.encode(Charset.forName("UTF-8"))
+				.build()
+				.toUri();
+		
+		log.info("uri : {}", uri);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		RequestEntity<Void> req = RequestEntity
+				.get(uri)
+				.header("X-Naver-Client-Id","EitF_WdGGp8oMpYq1D8N")
+				.header("X-Naver-Client-Secret","vfb1ckAOGn")
+				.build();
+				
+		ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+		
+		return result.getBody();
+	}
 	
 	@GetMapping("/hello")
 	public User hello(@RequestParam String name, @RequestParam int age) {
